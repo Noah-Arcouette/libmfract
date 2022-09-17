35c1e0d27b31afaea87116d372a4093d7a7edd8b
Noah Arcouette <kcu9626@gmail.com>
2022-09-16 21:19:33 -0600

    add optimization to addition - all

--- src/addition.c
+++ src/addition.c
@@ -4,43 +4,43 @@
 
 fraction32 f32add (fraction32 x, fraction32 y)
 {
-	uint16_t xa = (uint16_t)(x>>sizeof(fraction32)*4);
-	uint16_t xb = (uint16_t)x;
+	const uint16_t xa = (uint16_t)(x>>sizeof(fraction32)*4);
+	const uint16_t xb = (uint16_t)x;
 
-	uint16_t ya = (uint16_t)(y>>sizeof(fraction32)*4);
-	uint16_t yb = (uint16_t)y;
+	const uint16_t ya = (uint16_t)(y>>sizeof(fraction32)*4);
+	const uint16_t yb = (uint16_t)y;
 
-	xa  = (xa * yb) + (ya * xb);
-	xb *= yb;
-
-	return Fraction(xa, xb, fraction32);
+	return (
+		(uint32_t)((xa * yb) + (ya * xb)
+	) << (sizeof(fraction32)*4))
+	+ (uint32_t)(xb*yb);
 }
 
 fraction16 f16add (fraction16 x, fraction16 y)
 {
-	uint8_t xa = (uint8_t)(x>>sizeof(fraction16)*4);
-	uint8_t xb = (uint8_t)x;
+	const uint8_t xa = (uint8_t)(x>>sizeof(fraction16)*4);
+	const uint8_t xb = (uint8_t)x;
 
-	uint8_t ya = (uint8_t)(y>>sizeof(fraction16)*4);
-	uint8_t yb = (uint8_t)y;
+	const uint8_t ya = (uint8_t)(y>>sizeof(fraction16)*4);
+	const uint8_t yb = (uint8_t)y;
 
-	xa  = (xa * yb) + (ya * xb);
-	xb *= yb;
-
-	return Fraction(xa, xb, fraction16);
+	return (
+		(uint16_t)((xa * yb) + (ya * xb)
+	) << (sizeof(fraction16)*4)) 
+	+ (uint16_t)(xb*yb);
 }
 
 fraction64 f64add (fraction64 x, fraction64 y)
 {
-	uint32_t xa = (uint32_t)(x>>sizeof(fraction64)*4);
-	uint32_t xb = (uint32_t)x;
+	const uint32_t xa = (uint32_t)(x>>sizeof(fraction64)*4);
+	const uint32_t xb = (uint32_t)x;
 
-	uint32_t ya = (uint32_t)(y>>sizeof(fraction64)*4);
-	uint32_t yb = (uint32_t)y;
+	const uint32_t ya = (uint32_t)(y>>sizeof(fraction64)*4);
+	const uint32_t yb = (uint32_t)y;
 
-	xa  = (xa * yb) + (ya * xb);
-	xb *= yb;
-
-	return Fraction(xa, xb, fraction64);
+	return (
+		(uint64_t)((xa * yb) + (ya * xb)
+	) << (sizeof(fraction64)*4)) 
+	+ (uint64_t)(xb*yb);
 }
 
